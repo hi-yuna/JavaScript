56ae0f107187f1f00bc3bd469a384273461887e7
yuna <ouna0223@naver.com>
2021-06-10 01:11:31 +0900

    함수(function) 공부

---
+++ 01.입문/Function02_TemplateLiteral.js
@@ -0,0 +1,6 @@
+function hello(name) {
+    return `Hello ${name}!`;
+}
+
+const result = hello('yuna');
+console.log(result);
---
+++ 01.입문/Function03_화살표함수.js
@@ -0,0 +1,13 @@
+const add = (a, b) => {
+    return a + b;
+}
+
+const sum = add(1,2);
+console.log(sum);
+
+
+const hello = (name) => {
+    console.log(`Hello!, ${name}!`);
+}
+
+hello('yuna');

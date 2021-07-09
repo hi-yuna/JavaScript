594d1d823fc5c6a7df1bd475d21832528100f15d
yuna <ouna0223@naver.com>
2021-07-10 01:26:46 +0900

    Quiz1 풀기

---
+++ 06.Doit_객체/js/quiz1.js
@@ -0,0 +1,11 @@
+var member = {
+  name: "홍길동",
+  id: "123",
+  age: 25,
+  address: "서울",
+};
+
+document.write("<h2>" + member.name + "</h2>");
+document.write("<ul><li>id : " + member.id + "</li>");
+document.write("<li>나이 : " + member.age + "</li>");
+document.write("<li>주소 : " + member.address + "</li></ul>");

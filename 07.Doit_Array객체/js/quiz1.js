d1234c45b97316e7f6b3004aa08d0b4b14897ece
yuna <ouna0223@naver.com>
2021-07-10 17:33:25 +0900

    Quiz1 풀기

---
+++ 07.Doit_Array객체/js/quiz1.js
@@ -0,0 +1,17 @@
+var numbers = [2, 4, 6, 8, 10];
+
+var sum = 0;
+for (var i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+numbers.push(sum);
+showArray(numbers);
+
+function showArray(arr) {
+  var str = "<table><tr>";
+  for (var i = 0; i < arr.length; i++) {
+    str += "<td>" + arr[i] + "</td>";
+  }
+  str += "</tr></table>";
+  document.write(str);
+}

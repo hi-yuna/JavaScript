91e6ff8a2642450d8938e93cb23f8b48764cdcc5
yuna <ouna0223@naver.com>
2021-07-11 18:39:54 +0900

    Quiz1 풀기

---
+++ 08.Doit_DOM/js/quiz1.js
@@ -0,0 +1,6 @@
+var p = document.querySelector("#myText");
+p.addEventListener("click", function () {
+  p.style.fontSize = "20px";
+  p.style.color = "blue";
+  p.style.backgroundColor = "#ccc";
+});

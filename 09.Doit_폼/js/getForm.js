36e6c3156afc3d8992f2f67f619e9c26d9015ae0
yuna <ouna0223@naver.com>
2021-07-12 14:16:08 +0900

    선택목록 및 옵션항목에 접근하기

---
+++ 09.Doit_폼/js/getForm.js
@@ -0,0 +1,6 @@
+var selectMenu = document.testForm.major;
+
+function displaySelect() {
+  var selectedText = selectMenu.options[selectMenu.selectedIndex].innerText;
+  alert("[ " + selectedText + " ]를 선택했습니다.");
+}

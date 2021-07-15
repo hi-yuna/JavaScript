50b97834b4f3fa72393c9414b865ed6929640490
yuna <ouna0223@naver.com>
2021-07-15 23:42:48 +0900

    JS복습_Object

---
+++ 11.Review/02.object/object01.js
@@ -0,0 +1,5 @@
+function info() {
+  document.getElementById("bName").innerText = navigator.appName;
+  document.getElementById("bVersion").value = navigator.appVersion;
+  document.getElementById("bOnLine").innerText = navigator.onLine;
+}
---
+++ 11.Review/02.object/object02.js
@@ -0,0 +1,8 @@
+function google() {
+  location.href = "http://www.google.com";
+}
+
+function site() {
+  var site = document.getElementById("site").value;
+  location.href = site;
+}
